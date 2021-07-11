acdfd71f979c9bae09e635ebbe252672956f3c74
Adarsh Raj <cattyadarsh@gmail.com>
2021-07-11 17:53:07 +0530

    no message

--- vaccineFinder/urls.py
+++ vaccineFinder/urls.py
@@ -18,11 +18,19 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.basePage,name="basePage"),
+    path(r'',views.basePage,name="basePage"),
     path('findVaccination/',include('findVaccine.urls')),
-    path('naradMuni_News/',include('naradMuni.urls'))
+    path('naradMuni_News/',include('naradMuni.urls')),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
